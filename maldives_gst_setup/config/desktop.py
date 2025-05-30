8a4c4a1bb543ea3696e541fe83262d1fadd82693
Shuhain Ismail <shuhain.ismail@gmail.com>
2025-05-30 22:14:19 +0000

    develop

---
+++ maldives_gst_setup/config/desktop.py
@@ -0,0 +1,16 @@
+app_name = "maldives_gst_setup"
+app_title = "Maldives Gst Setup"
+app_publisher = "Your Name"
+app_description = "..."
+app_icon = "octicon octicon-file-directory"
+app_color = "blue"
+app_email = "you@example.com"
+app_license = "MIT"
+
+app_include_js = []
+app_include_css = []
+
+# Add this if you want to show a module with pages
+app_name = "maldives_gst_setup"
+
+#app_logo_url = "/assets/maldives_gst_setup/images/logo.png"
---
+++ maldives_gst_setup/www/setup_wizard.py
@@ -0,0 +1,5 @@
+import frappe
+from frappe import _
+
+def get_context(context):
+    context.title = _("Maldives GST Setup Wizard")
